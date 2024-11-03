8d0663d1d10e09eb9a9e146f1843a617d0d54d3e
JamieOgle27 <jamieogle27@gmail.com>
2024-11-03 23:39:14 +0000

    Added ship
    Added ship image
    Added some basic movement
    Code refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
 import sys
-
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
@@ -14,21 +14,43 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alein Invasion")
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            #Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
+
+    def _check_events(self):
+    # watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+
+
+
+    def _update_screen(self):
+        #Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
 if __name__ =='__main__':
     # Make a game instance, and run the game.
