4834cc6f4472278894de4923746528542ae5f0ff
JamieOgle27 <jamieogle27@gmail.com>
2024-11-04 19:29:07 +0000

    Fullscreen mode
    Added fullscreen option
    allowed user to press 'q' to quit the game

--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ class Settings:
         # Screen settings
         self.screen_width = 900
         self.screen_height = 600
-        self.bg_color = (120, 230, 230)
+        self.bg_color = (255, 255, 255)
+        self.fullscreen = False
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 1.1
